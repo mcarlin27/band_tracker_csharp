c28b12a2a669db11b94fb9b88cb032e5acec610e
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-18 14:47:21 -0700

    add functionality to view individual venue pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -20,6 +20,39 @@ namespace BandTracker.Objects
         model.Add("form-type", "new-band");
         return View["form.cshtml", model];
       }; //returns form to add new band
+      Post["/bands/new"] = _ => {
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        Band band = new Band(Request.Form["band-name"]);
+        band.Save();
+        model.Add("listBands", Band.GetAll());
+        model.Add("listVenues", Venue.GetAll());
+        model.Add("newBand", band);
+        model.Add("show-info", "new-band-info");
+        return View["index.cshtml", model];
+      }; //posts from form adding new band
+      Get["/venues/new"] = _ => {
+        Dictionary<string, string> model = new Dictionary<string, string>{};
+        model.Add("form-type", "new-venue");
+        return View["form.cshtml", model];
+      }; //returns form to add new venue
+      Post["/venues/new"] = _ => {
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        Venue venue = new Venue(Request.Form["venue-name"], Request.Form["venue-city"]);
+        venue.Save();
+        model.Add("listBands", Band.GetAll());
+        model.Add("listVenues", Venue.GetAll());
+        model.Add("newVenue", venue);
+        model.Add("show-info", "new-venue-info");
+        return View["index.cshtml", model];
+      }; //posts from form adding new venue
+      Get["/venues/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Venue selectedVenue = Venue.Find(parameters.id);
+        List<Band> venueBands = selectedVenue.GetBands();
+        model.Add("venue", selectedVenue);
+        model.Add("bands", venueBands);
+        return View["venue.cshtml", model];
+      }; //retrieves individual venue pages
 
     }
   }
