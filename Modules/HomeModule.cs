b0a6e53b794d3e4507eff7331359cddddda4d5cb
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-18 14:09:01 -0700

    add form and Get route to allow user to add a new band

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -14,7 +14,13 @@ namespace BandTracker.Objects
         model.Add("listVenues", Venue.GetAll());
         model.Add("show-info", null);
         return View["index.cshtml", model];
-      };
+      }; //homepage with lists of venues/bands, buttons to add venue/band
+      Get["/bands/new"] = _ => {
+        Dictionary<string, string> model = new Dictionary<string, string>{};
+        model.Add("form-type", "new-band");
+        return View["form.cshtml", model];
+      }; //returns form to add new band
+
     }
   }
 }
